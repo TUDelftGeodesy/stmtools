3f622bf4a27fffcd3880a06c36fc1222d702c088
Pranav Chandramouli <cpranav93@yahoo.com>
2022-10-27 12:43:52 +0200

    added threshold subset

--- stmat.py
+++ stmat.py
@@ -17,13 +17,25 @@ class SpaceTimeMatrix:
         self._obj = self._obj.assign_attrs(metadata)
         return self._obj.copy()
 
-    def subset(self, **kwargs):
+    def subset(self, method, **kwargs):
         # To be implemented
         # Spatial query, by polygon, bounding box (priority) 
         # Threshold Query
         # Density Query
-
-        return None
+        match method:   #Match statements available only from python 3.10 onwards
+            case 'threshold':
+                check_threshold_kwargs(**kwargs) #Check is all required kwargs are available
+                if kwargs['threshold'][0] == '<':
+                    str_parts = kwargs['threshold'].partition('<')
+                    check_mult_relops(str_parts[2]) #Check to ensure multiple relational operators are not present
+                    data_xr_subset= self._obj.where(self._obj[kwargs['var']] < float(str_parts[2]), drop=True)
+                elif kwargs['threshold'][0] == '>':
+                    str_parts = kwargs['threshold'].partition('>')
+                    check_mult_relops(str_parts[2])
+                    data_xr_subset= self._obj.where(self._obj[kwargs['var']] > float(str_parts[2]), drop=True)
+                else:
+                    raise Exception('Suitable relational operator not found! Please check input') 
+        return data_xr_subset
 
     def from_stack(self, stack_obj):
         # Make  from a Stack Object
@@ -37,9 +49,17 @@ class SpaceTimeMatrix:
 
         return None
 
+def check_mult_relops(string):
+    relops = ['<','>']
+    for i in relops:
+        if i in string:
+            raise Exception('Multiple relational operators found! Please check input')
 
-
-        
+def check_threshold_kwargs(**kwargs):
+    req_kwargs=['var','threshold']
+    for i in req_kwargs:
+        if i not in kwargs:
+            raise Exception('Missing expected keyword argument: %s'%i)
  
 
 
