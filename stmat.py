af308fc7193b78d33477a7cd17e8be230f3fb59b
Pranav Chandramouli <cpranav93@yahoo.com>
2022-11-21 15:35:59 +0100

    Added density subsetting - draft version, to stmat.py. Also added example in stm_subset_example.

--- stmat.py
+++ stmat.py
@@ -1,4 +1,10 @@
 import xarray as xr
+from rasterio import features
+from rasterio import enums
+import math
+import numpy as np
+import geopandas as gpd
+import affine
 
 
 # Inspiration: 
@@ -35,6 +41,15 @@ class SpaceTimeMatrix:
                     data_xr_subset= self._obj.where(self._obj[kwargs['var']] > float(str_parts[2]), drop=True)
                 else:
                     raise Exception('Suitable relational operator not found! Please check input') 
+            case 'density':
+                check_density_kwargs(**kwargs) #Check for all require kwargs
+                gdf = gpd.GeoDataFrame(self._obj['points'], geometry=gpd.points_from_xy(self._obj[kwargs['x']],self._obj[kwargs['y']]))
+                grid_cell  = ((shapes) for shapes in zip(gdf.geometry, gdf.index))
+                out_x = math.ceil((max(self._obj[kwargs['x']]) - min(self._obj[kwargs['x']]))/kwargs['dx'])
+                out_y = math.ceil((max(self._obj[kwargs['y']]) - min(self._obj[kwargs['y']]))/kwargs['dy'])
+                raster = features.rasterize(shapes=grid_cell, out_shape=[out_x, out_y], fill=np.NAN, all_touched= True, default_value=1, transform=affine.Affine.from_gdal(min(self._obj[kwargs['x']]), kwargs['dx'], 0.0, max(self._obj[kwargs['y']]), 0.0, -1*kwargs['dy']))
+                subset = [item for item in np.unique(raster) if not(math.isnan(item)) == True]
+                data_xr_subset = self._obj.sel(points=subset)
         return data_xr_subset
 
     def from_stack(self, stack_obj):
@@ -61,6 +76,16 @@ def check_threshold_kwargs(**kwargs):
         if i not in kwargs:
             raise Exception('Missing expected keyword argument: %s'%i)
  
+
+def check_density_kwargs(**kwargs):
+    req_kwargs=['x','y','dx','dy' ]
+    for i in req_kwargs:
+        if i not in kwargs:
+            raise Exception('Missing expected keyword argument: %s'%i)
+        if i in ['dx', 'dy']:
+            if not isinstance(kwargs[i], float):
+                raise Exception('Keyword argument %s should be an floating point number'%i)
+  
 
 
   
