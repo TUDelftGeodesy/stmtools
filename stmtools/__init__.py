f671d3533e9315677ef5ab2ca93d85d9b416c04f
Ou Ku <o.ku@esciencecenter.nl>
2023-11-07 15:55:38 +0100

    apply ruff formatting

--- stmtools/__init__.py
+++ stmtools/__init__.py
@@ -1,2 +1,4 @@
-import stmtools.stm
+from stmtools import stm
 from stmtools._io import from_csv
+
+__all__ = ("stm", "from_csv")
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,22 +1,19 @@
-import xarray as xr
-import dask.array as da
-import numpy as np
 import pytest
 from stmtools import utils
-from tests.test_stm import stmat
 
-class TestHasProperty():
+
+class TestHasProperty:
     def test_has_property_str(self, stmat):
-        assert utils._has_property(stmat, 'phase')
+        assert utils._has_property(stmat, "phase")
 
     def test_has_property_list(self, stmat):
-        assert utils._has_property(stmat, ['phase', 'amplitude'])
+        assert utils._has_property(stmat, ["phase", "amplitude"])
 
     def test_has_property_tuple(self, stmat):
-        assert utils._has_property(stmat, ('phase', 'amplitude'))
-    
+        assert utils._has_property(stmat, ("phase", "amplitude"))
+
     def test_has_not(self, stmat):
-        assert not(utils._has_property(stmat, ['phase', 'no_exists']))
+        assert not (utils._has_property(stmat, ["phase", "no_exists"]))
 
     def test_incorrect_type(self, stmat):
         with pytest.raises(ValueError):
