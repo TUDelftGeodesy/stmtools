b3101c83b5ddc474193c7945ccb732046223db73
SarahAlidoost <f.alidoost@esciencecenter.nl>
2024-03-21 13:17:24 +0100

    add util functions for cropping and unstack operations

--- stmtools/utils.py
+++ stmtools/utils.py
@@ -1,3 +1,4 @@
+import xarray as xr
 from collections.abc import Iterable
 
 
@@ -8,3 +9,51 @@ def _has_property(ds, keys: str | Iterable):
         return set(keys).issubset(ds.data_vars.keys())
     else:
         raise ValueError(f"Invalid type of keys: {type(keys)}.")
+
+
+def crop(ds, other, buffer):
+    """Crop the other to a given buffer around ds.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset | xarray.DataArray
+        Dataset to crop to.
+    other : xarray.Dataset | xarray.DataArray
+        Dataset to crop.
+    buffer : dict
+        A dictionary with the buffer values for each dimension.
+
+    Returns
+    -------
+    xarray.Dataset
+        Cropped dataset.
+    """
+    if isinstance(ds, xr.DataArray):
+        ds = ds.to_dataset()
+
+    if isinstance(other, xr.DataArray):
+        other = other.to_dataset()
+
+    if not isinstance(buffer, dict):
+        raise ValueError(f"Invalid type of buffer: {type(buffer)}.")
+    for coord in buffer.keys():
+        if coord not in ds.coords.keys():
+            raise ValueError(f"coordinate '{coord}' not found in ds.")
+        if coord not in other.coords.keys():
+            raise ValueError(f"coordinate '{coord}' not found in other.")
+
+    other = unstack(other)
+    for coord in buffer.keys():
+        coord_min = ds[coord].min() - buffer[coord]
+        coord_max = ds[coord].max() + buffer[coord]
+        other = other.sel({coord: slice(coord_min, coord_max)})
+    return other
+
+
+def unstack(ds):
+    for dim in ds.dims:
+        if dim not in ds.coords:
+            indexer = {dim: [coord for coord in ds.coords if dim in ds[coord].dims]}
+            ds = ds.set_index(indexer)
+            ds = ds.unstack(dim)
+    return ds
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,8 @@
 import pytest
+import dask.array as da
+import numpy as np
+import pandas as pd
+import xarray as xr
 
 from stmtools import utils
 
@@ -19,3 +23,102 @@ class TestHasProperty:
     def test_incorrect_type(self, stmat):
         with pytest.raises(ValueError):
             utils._has_property(stmat, 1)
+
+@pytest.fixture
+def meteo_points():
+    n_times = 20
+    n_locations = 50
+    lon_values = np.arange(n_locations)
+    lat_values = np.arange(n_locations)
+    time_values = pd.date_range(start='2021-01-01', periods=n_times)
+
+    return xr.Dataset(
+        data_vars=dict(
+            temperature=(["space", "time"], da.arange(n_locations * n_times).reshape((n_locations, n_times))),
+        ),
+        coords=dict(
+            lon=(["space"], lon_values),
+            lat=(["space"], lat_values),
+            time=(["time"], time_values),
+        ),
+    ).unify_chunks()
+
+
+
+@pytest.fixture
+def meteo_raster():
+    n_times = 20
+    n_locations = 50
+    lon_values = np.arange(n_locations)
+    lat_values = np.arange(n_locations)
+    time_values = pd.date_range(start='2021-01-01', periods=n_times)
+    # add x and y values
+    x_values = np.arange(n_locations)
+    y_values = np.arange(n_locations)
+
+    return xr.Dataset(
+        data_vars=dict(
+            temperature=(["lon", "lat", "time"], da.arange(n_locations * n_locations * n_times).reshape((n_locations, n_locations, n_times))),
+        ),
+        coords=dict(
+            lon=(["lon"], lon_values),
+            lat=(["lat"], lat_values),
+            x=(["lon"], x_values),
+            y=(["lat"], y_values),
+            time=(["time"], time_values),
+        ),
+    ).unify_chunks()
+
+@pytest.fixture
+def stmat():
+    npoints = 10
+    ntime = 5
+    return xr.Dataset(
+        data_vars=dict(
+            amplitude=(
+                ["space", "time"],
+                da.arange(npoints * ntime).reshape((npoints, ntime)),
+            ),
+            phase=(
+                ["space", "time"],
+                da.arange(npoints * ntime).reshape((npoints, ntime)),
+            ),
+            pnt_height=(
+                ["space"],
+                da.arange(npoints),
+            ),
+        ),
+        coords=dict(
+            lon=(["space"], da.arange(npoints)),
+            lat=(["space"], da.arange(npoints)),
+            time=(["time"], pd.date_range(start='2021-01-02', periods=ntime)),
+        ),
+    ).unify_chunks()
+
+class TestCrop:
+    def test_crop_points(self, stmat, meteo_points):
+        buffer = {"lon": 1, "lat": 1, "time": 1}
+        cropped = utils.crop(stmat, meteo_points, buffer)
+        # check min and max values of coordinates
+        assert cropped.lon.min() == 0
+        assert cropped.lon.max() == 10
+        assert cropped.lat.min() == 0
+        assert cropped.lat.max() == 10
+        assert cropped.time.min() == pd.Timestamp("2021-01-02")
+        assert cropped.time.max() == pd.Timestamp("2021-01-06")
+
+    def test_crop_raster(self, stmat, meteo_raster):
+        buffer = {"lon": 1, "lat": 1, "time": 1}
+        cropped = utils.crop(stmat, meteo_raster, buffer)
+        # check min and max values of coordinates
+        assert cropped.lon.min() == 0
+        assert cropped.lon.max() == 10
+        assert cropped.lat.min() == 0
+        assert cropped.lat.max() == 10
+        assert cropped.time.min() == pd.Timestamp("2021-01-02")
+        assert cropped.time.max() == pd.Timestamp("2021-01-06")
+
+    def test_all_operations_lazy(self, stmat, meteo_raster):
+        buffer = {"lon": 1, "lat": 1, "time": 1}
+        cropped = utils.crop(stmat, meteo_raster, buffer)
+        assert isinstance(cropped.temperature.data, da.Array)
